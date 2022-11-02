364553ae9766b3528dc311ab53b47bd0c11a7454
heitorborbamarini <rm92976@fiap.com.br>
2022-11-02 11:18:07 -0300

    Criando componente Item e estilizando o Extrato

--- src/Components/Extrato/index.jsx
+++ src/Components/Extrato/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import {Box, Botao} from '../../Components/UI';
 import {extratoLista} from '../../info';
+import Items from '../Items';
 
 const Extrato = () => {
 
     return (
         <Box>
-            {extratoLista.updates.map(({Id, type, from, value, date}) => {
+            {extratoLista.updates.map(({id, type, from, value, date}) => {
 return(
-     <div   key={Id}>
-        <div> {type}</div>  
-        <div>{from}</div> 
-    </div>
+
+    <Items key={id} type={type} from={from}
+    value={value} date={date} />
     );
  })} 
 
