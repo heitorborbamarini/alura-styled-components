a7f01b98081d6612b3d8ccf0955a164f2f56df98
heitorborbamarini <rm92976@fiap.com.br>
2022-11-04 09:35:03 -0300

    Criando botão para troca de tema claro pra escuro

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React , {useState} from "react";
 import {GlobalStyle} from './Components/GlobalStyle';
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 
 import {ThemeProvider} from 'styled-components';
   import {temaClaro, temaEscuro} from './Components/UI/Temas';
+import { BtnTema } from "./Components/UI";
+import SwitcherTema from "./Components/SwitcherTema";
 
 
 function App() {
+  const [tema, setTema] = useState(true);
+
+  const toggleTema = () => {
+    setTema((tema) => !tema);
+  };
+
   return (
-    
-    <ThemeProvider theme={temaEscuro}>
+
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
     <GlobalStyle />
+    <BtnTema onClick={toggleTema}>
+      <SwitcherTema tema={tema}/>
+    </BtnTema>
       <Cabecalho />
-      <Container />
+      <Container />npm
     </ThemeProvider>
     
   );
